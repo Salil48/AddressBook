861626a0b47e13522e0c11de344fe8e07d3e611f
“Salil48” <salilchharia@gmail.com>
2023-01-05 15:27:34 +0530

    Ability to create a Contacts in Address Book

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args) 
         {
             Console.WriteLine("Welcome to Address Book System");
+
+            UC1_CreateContacts.PersonDetails();
         }
     }
 
