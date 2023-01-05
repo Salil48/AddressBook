a60e865409ca6a86c3d60a39a1af028bffadbdc9
“Salil48” <salilchharia@gmail.com>
2023-01-05 17:08:39 +0530

    Ability to add multiple person to Address Book

--- Program.cs
+++ Program.cs
@@ -4,10 +4,39 @@
     {
         public static void Main(string[] args)
         {
-            AddressBookSystem.UC4_Rem.GetCustomer();
-            AddressBookSystem.UC4_Rem.Modify();
-            AddressBookSystem.UC4_Rem.ListingPeople();
-            AddressBookSystem.UC4_Rem.RemovePeople();
+            Console.WriteLine("Welcome To Address Book System!");
+            Console.WriteLine("1.Enter to add the details");
+            Console.WriteLine("2.Enter to modify the details");
+            Console.WriteLine("3.Listing the details..");
+            Console.WriteLine("4.Remove the details");
+            Console.WriteLine("Enter a option");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    AddressBookSystem.UC5_Multiple.GetCustomer();
+                    AddressBookSystem.UC5_Multiple.ListingPeople();
+                    break;
+                case "2":
+                    AddressBookSystem.UC5_Multiple.GetCustomer();
+                    AddressBookSystem.UC5_Multiple.Modify();
+                    AddressBookSystem.UC5_Multiple.ListingPeople();
+                    break;
+                case "3":
+                    AddressBookSystem.UC5_Multiple.GetCustomer();
+                    AddressBookSystem.UC5_Multiple.ListingPeople();
+                    break;
+                case "4":
+                    AddressBookSystem.UC5_Multiple.GetCustomer();
+                    AddressBookSystem.UC5_Multiple.RemovePeople();
+
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
+
+            }
+
+
 
         }
     }
